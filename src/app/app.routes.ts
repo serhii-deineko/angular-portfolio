686e06aa693a3001c5f1b40128c47e7810d72294
Serhii Deineko <serhii.deineko@gmail.com>
2025-10-23 23:52:41 +0200

    Add routing for about, projects, experience, and contact pages with RedirectGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,32 @@ import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { ProjectDetailComponent } from "./projects/project-detail/project-detail.component";
 import { EmojiSeekerComponent } from "./app-emoji";
+import { RedirectGuard } from "./guards/redirect.guard";
 
 export const routes: Routes = [
 	{
 		path: "",
 		component: HomeComponent
+	},
+	{
+		path: "about",
+		component: HomeComponent,
+		canActivate: [RedirectGuard]
+	},
+	{
+		path: "projects",
+		component: HomeComponent,
+		canActivate: [RedirectGuard]
+	},
+	{
+		path: "experience",
+		component: HomeComponent,
+		canActivate: [RedirectGuard]
+	},
+	{
+		path: "contact",
+		component: HomeComponent,
+		canActivate: [RedirectGuard]
 	},
 	{
 		path: "emoji-seeker",
